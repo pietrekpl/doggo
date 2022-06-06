b14a3fbb73531275f5a390ea6a3eba4f2e5af64e
Piotr Ludynia <pietrekpl18@gmail.com>
2022-06-06 22:28:12 +0200

    initial Commit

---
+++ src/main/java/com/example/doggo/DoggoApplication.java
@@ -0,0 +1,13 @@
+package com.example.doggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doggo/DoggoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
